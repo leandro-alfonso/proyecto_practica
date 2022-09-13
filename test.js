b96fdd329694be77926e19262d0652d717e97564
leandro alfonso <leandroalfonso645@gmail.com>
2022-09-13 19:00:17 -0300

    mi primer commit del proyecto prueba

--- test.js
+++ test.js
@@ -3,6 +3,12 @@ let hacer = document.getElementById("test2")
 let pancho = document.getElementById("test3")
 let paty = document.getElementById("test4")
 let testss = document.getElementById("tests")
+// capturando img
+let imgPerro = document.getElementById("imgContento")
+let imgDormir = document.getElementById("imgPensar")
+let imgPizza = document.getElementById("imgRicos")
+let imgEstacion = document.getElementById("imgPensando")
+// alert de inicio
  Swal.fire({
     html: `<h1 class="swalTitulo">Hola</h1>
             <p class="swalParrafo">acontinuacion haremos un pequeño test para saber cuanto me conoces</p>`,
@@ -19,6 +25,8 @@ let testss = document.getElementById("tests")
         alert("error")
     }
 })
+
+// arrays que guarda respuestas 
 const incorrectoAnimal = []
 const incorrectoComidas = []
 const incorrectoHace = []
@@ -28,7 +36,7 @@ const summer = []
 const dos = []
 const food = []
 
-// empezando test
+// empezando test : captura de botones
 // animales 
 let perro = document.getElementById("perro")
 console.log(perro)
@@ -38,6 +46,33 @@ gato.addEventListener("click",incorrectoAnimales)
 let tortuga = document.getElementById("Tortuga")
 tortuga.addEventListener("click",incorrectoAnimales)
 
+// que me gusta hacer
+let dormir = document.getElementById("dormir")
+dormir.addEventListener("click",dormirse)
+let comer = document.getElementById("comer")
+comer.addEventListener("click",incorrectoHacer)
+let salir = document.getElementById("salir")
+salir.addEventListener("click",incorrectoHacer)
+
+// estaciones
+let verano = document.getElementById("verano")
+verano.addEventListener("click",veranos)
+let invierno = document.getElementById("invierno")
+invierno.addEventListener("click",incorrectoEstaciones)
+let otoño = document.getElementById("otoño")
+otoño.addEventListener("click",incorrectoEstaciones)
+let primavera = document.getElementById("primavera")
+primavera.addEventListener("click",incorrectoEstaciones)
+
+// comida
+let ensalada = document.getElementById("ensalada")
+ensalada.addEventListener("click",incorrectoComida)
+let pizza = document.getElementById("pizza")
+pizza.addEventListener("click",pizzas)
+let hamburguesa = document.getElementById("hamburguesa")
+hamburguesa.addEventListener("click",incorrectoComida)
+let filetes= document.getElementById("filetes")
+filetes.addEventListener("click",incorrectoComida)
 
 // FUNCION RESPUESTA CORRECTA
 
@@ -57,13 +92,89 @@ function perros(){
             let variablePerro = "perro"
             dog.push(variablePerro)
             console.log(dog)
+            imgPerro.src = "../img/perro.png"
+            rata.style.display = "none";
         }else{
             alert("error")
         }
     })
 }
 
-// FUNCIONES SOBRE RESPUESTA INCORRECTA
+function dormirse(){
+    Swal.fire({
+        icon: 'success',
+        html: `<h1 class="swalTitulo">Correcto</h1>
+                <p class="swalParrafo">Nada mas lindo que una siestita</p>`,
+        imageUrl: '../img/dormir.png',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        confirmButtonText: "seguir",
+      }).then((result)=>{
+        console.log(result)
+        if (result.isConfirmed){
+            let variableDormir = "dormir"
+            dos.push(variableDormir)
+            console.log(dos)
+            imgPensar.src = "../img/dormir.png"
+            hacer.style.display = "none";
+        }else{
+            alert("error")
+        }
+    })
+}
+
+function pizzas(){
+    Swal.fire({
+        icon: 'success',
+        html: `<h1 class="swalTitulo">Correcto</h1>
+                <p class="swalParrafo">Lo mas rico que existe</p>`,
+        imageUrl: '../img/comida.png',
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Custom image',
+        confirmButtonText: "seguir",
+      }).then((result)=>{
+        console.log(result)
+        if (result.isConfirmed){
+            let variablePizza = "pizza"
+            food.push(variablePizza)
+            console.log(food)
+            pancho.style.display = "none";
+            imgPizza.src = "../img/comida.png"
+        }else{
+            alert("error")
+        }
+    })
+}
+
+function veranos(){
+    Swal.fire({
+        icon: 'success',
+        html: `<h1 class="swalTitulo">Correcto</h1>
+                <p class="swalParrafo">La mejor estacion</p>`,
+        imageUrl: '../img/verano.png',
+        imageWidth: 400,
+        imageHeight: 250,
+        imageAlt: 'Custom image',
+        confirmButtonText: "seguir",
+      }).then((result)=>{
+        console.log(result)
+        if (result.isConfirmed){
+            let variableVerano = "verano"
+            summer.push(variableVerano)
+            console.log(summer)
+            paty.style.display = "none";
+            imgEstacion.src = "../img/verano.png"
+
+        }else{
+            alert("error")
+        }
+    })
+}
+
+
+// FUNCIONES SOBRE 1 RESPUESTA INCORRECTA
 function incorrectoAnimales(){
     Swal.fire({
         icon: 'error',
@@ -79,6 +190,7 @@ function incorrectoAnimales(){
             let error = "respuesta incorrecta"
             incorrectoAnimal.push(error)
             rata.style.display = "none";
+            imgPerro.src = "../img/mal.png"
         }else{
             alert("error")
         }
@@ -100,6 +212,7 @@ function incorrectoHacer(){
             let error = "respuesta incorrecta"
            incorrectoHace.push(error)
             hacer.style.display = "none";
+            imgPensar.src = "../img/mal.png"
         }else{
             alert("error")
         }
@@ -122,6 +235,7 @@ function incorrectoComida(){
             let error = "respuesta incorrecta"
             incorrectoComidas.push(error)
             pancho.style.display = "none";
+            imgPizza.src = "../img/mal.png"
         }else{
             alert("error")
         }
@@ -144,6 +258,7 @@ function incorrectoEstaciones(){
             let error = "respuesta incorrecta"
             incorrectoEstacion.push(error)
             paty.style.display = "none";
+            imgEstacion.src = "../img/mal.png"
         }else{
             alert("error")
         }
@@ -151,105 +266,104 @@ function incorrectoEstaciones(){
       
 }
 
-// estaciones
-let verano = document.getElementById("verano")
-verano.addEventListener("click",veranos)
-let invierno = document.getElementById("invierno")
-invierno.addEventListener("click",incorrectoEstaciones)
-let otoño = document.getElementById("otoño")
-otoño.addEventListener("click",incorrectoEstaciones)
-let primavera = document.getElementById("primavera")
-primavera.addEventListener("click",incorrectoEstaciones)
-
-function veranos(){
+// funciones resultados de todas las respuestas correctas
+function resultCorrecto(){
     Swal.fire({
-        icon: 'success',
-        html: `<h1 class="swalTitulo">Correcto</h1>
-                <p class="swalParrafo">La mejor estacion</p>`,
-        imageUrl: '../img/verano.png',
-        imageWidth: 400,
-        imageHeight: 250,
+        html: `<h1 class="swalTitulo">Felicidades</h1>
+                <p class="swalParrafo">Demostraste conocerme</p>`,
+        imageUrl: '../img/enamorado.png',
+        imageWidth: 360,
+        imageHeight: 290,
         imageAlt: 'Custom image',
-        confirmButtonText: "seguir",
-      }).then((result)=>{
-        console.log(result)
-        if (result.isConfirmed){
-            let variableVerano = "verano"
-            summer.push(variableVerano)
-            console.log(summer)
-        }else{
-            alert("error")
-        }
-    })
-}
-// comida
-let ensalada = document.getElementById("ensalada")
-ensalada.addEventListener("click",incorrectoComida)
-let pizza = document.getElementById("pizza")
-pizza.addEventListener("click",pizzas)
-let hamburguesa = document.getElementById("hamburguesa")
-hamburguesa.addEventListener("click",incorrectoComida)
-let filetes= document.getElementById("filetes")
-filetes.addEventListener("click",incorrectoComida)
-
-function pizzas(){
-    Swal.fire({
-        icon: 'success',
-        html: `<h1 class="swalTitulo">Correcto</h1>
-                <p class="swalParrafo">Lo mas rico que existe</p>`,
-        imageUrl: '../img/comida.png',
-        imageWidth: 400,
-        imageHeight: 270,
-        imageAlt: 'Custom image',
-        confirmButtonText: "seguir",
-      }).then((result)=>{
-        console.log(result)
-        if (result.isConfirmed){
-            let variablePizza = "pizza"
-            food.push(variablePizza)
-            console.log(food)
-        }else{
-            alert("error")
-        }
-    })
+        confirmButtonText: "vamos!",
+      })
 }
 
-// que me gusta hacer
-let dormir = document.getElementById("dormir")
-dormir.addEventListener("click",dormirse)
-let comer = document.getElementById("comer")
-comer.addEventListener("click",incorrectoHacer)
-let salir = document.getElementById("salir")
-salir.addEventListener("click",incorrectoHacer)
+// funciones con una respuesta incorrecta
 
-function dormirse(){
+function resultPrimer(){
     Swal.fire({
-        icon: 'success',
-        html: `<h1 class="swalTitulo">Correcto</h1>
-                <p class="swalParrafo">Nada mas lindo que una siestita</p>`,
+        html: `<h1 class="swalTitulo">CASI</h1>
+                <p class="swalParrafo">3 de 4 ... me agradan mas los perros</p>`,
+        imageUrl: '../img/resPerro.png',
+        imageWidth: 400,
+        imageHeight: 290,
+        imageAlt: 'Custom image',
+        confirmButtonText: "vamos!",
+      })
+}
+
+function resultSegundo(){
+    Swal.fire({
+        html: `<h1 class="swalTitulo">CASI</h1>
+                <p class="swalParrafo">3 de 4 , ...me gusta mas dormir</p>`,
         imageUrl: '../img/dormir.png',
         imageWidth: 400,
-        imageHeight: 200,
+        imageHeight: 290,
         imageAlt: 'Custom image',
-        confirmButtonText: "seguir",
-      }).then((result)=>{
-        console.log(result)
-        if (result.isConfirmed){
-            let variableDormir = "dormir"
-            dos.push(variableDormir)
-            console.log(dos)
-        }else{
-            alert("error")
-        }
-    })
+        confirmButtonText: "vamos!",
+      })
 }
+
+function resultTercero(){
+    Swal.fire({
+        html: `<h1 class="swalTitulo">CASI</h1>
+                <p class="swalParrafo">3 de 4 , ...me gusta mas la pizza</p>`,
+        imageUrl: '../img/comida.png',
+        imageWidth: 400,
+        imageHeight: 290,
+        imageAlt: 'Custom image',
+        confirmButtonText: "vamos!",
+      })
+}
+
+function resultCuarto(){
+    Swal.fire({
+        html: `<h1 class="swalTitulo">CASI</h1>
+                <p class="swalParrafo">3 de 4 , ...me gusta mas el verano</p>`,
+        imageUrl: '../img/verano.png',
+        imageWidth: 380,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+        confirmButtonText: "vamos!",
+      })
+}
+
+// funciones sobre 2 respuestas incorrectas
+function respIncorrecto(){
+    Swal.fire({
+        html: `<h1 class="swalTitulo">Que??</h1>
+                <p class="swalParrafo">2 de 2 , de verdad?</p>`,
+        imageUrl: '../img/que.png',
+        imageWidth: 360,
+        imageHeight: 290,
+        imageAlt: 'Custom image',
+        confirmButtonText: "vamos!",
+      })
+}
+
+// todas las respuestas incorrectas
+function todoIncorrecto(){
+    Swal.fire({
+        html: `<h1 class="swalTitulo">MAL</h1>
+                <p class="swalParrafo">Se nota que no me conoces lo suficiente</p>`,
+        imageUrl: '../img/molesto.png',
+        imageWidth: 360,
+        imageHeight: 290,
+        imageAlt: 'Custom image',
+        confirmButtonText: "vamos!",
+      })
+}
+
+// capturando boton resultado
+
 let result = document.getElementById("result")
 result.addEventListener("click",resultado)
 
+// funcion de resultado final
+
 function resultado(){
-    // for (let electricidad of dog){
-    //     let lean = electricidad
-    //     console.log(lean)
+
  let resp1 = dog
  let resp2 = dos
  let resp3 = food
@@ -259,48 +373,22 @@ function resultado(){
  let incorrecto3 = incorrectoComidas
  let incorrecto4 = incorrectoEstacion
         if (resp1 == 'perro' && resp2 == `dormir` && resp3==`pizza` && resp4 ==`verano`){
-            console.log(resp1)
-            alert("bien")
+            resultCorrecto()
         }else if (incorrecto1 == `respuesta incorrecta` && resp2 == `dormir` && resp3==`pizza` && resp4 ==`verano`){
-            alert("good")
+            resultPrimer()
         }else if(resp1 == 'perro' && incorrecto2 == `respuesta incorrecta` && resp3==`pizza` && resp4 ==`verano`){
-            alert("mal")
+            resultSegundo()
         }else if (resp1 == 'perro' && resp2 == `dormir` && incorrecto3 == `respuesta incorrecta` && resp4 ==`verano`){
-            alert("mal2")
+            resultTercero()
         }else if(resp1 == 'perro' && resp2 == `dormir` && resp3==`pizza` && incorrecto4 == `respuesta incorrecta`){
-            alert("mal3")
+            resultCuarto()
         }else if(incorrecto1 == `respuesta incorrecta` && incorrecto2 == `respuesta incorrecta` && resp3==`pizza` && resp4 ==`verano`){
-            alert("mal4")
+            respIncorrecto()
         }else if( resp1 == 'perro' && incorrecto2 == `respuesta incorrecta` && incorrecto3 == `respuesta incorrecta` && resp4 ==`verano`){
-            alert("mal5")
+            respIncorrecto()
         }else if(resp1 == 'perro' && resp2 == `dormir` && incorrecto3 == `respuesta incorrecta` && incorrecto4 == `respuesta incorrecta`){
-            alert(`mal6`)
+            respIncorrecto()
         }else{
-            alert("fin")
+            todoIncorrecto()
         }
     }
-
-// }
-
-// const respuest = []
-
-// class respuestas {
-//     constructor (respuestas1,respuestas2,respuestas3,respuestas4){
-//         this.respuestas1 = respuestas1
-//         this.respuestas2 = respuestas2
-//         this.respuestas3 = respuestas3
-//         this.respuestas4 = respuestas4
-//     }
-// }
-
-// const res1 = new respuestas (variablePerro,variableDormir,variablePizza,variableVerano)
-// respuest.push(res1)
-// console.log(respuest)
-
-// const arr = [1,2,3,4]
-
-// const arr2 = [...arr]
-
-// arr.forEach((numero)=>{
-//     arr2.push(numero)
-// })
